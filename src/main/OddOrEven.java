53364e2ad8b46f3e1c42e75d49b92057b417bb18
laurastrathern <96526409+laurastrathern@users.noreply.github.com>
2022-01-21 18:14:58 +0000

    Initial Commit: Add OddOrEven class file.

---
+++ src/main/OddOrEven.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.util.Scanner;
+
+public class OddOrEven {
+
+    public static boolean isEven(int n) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int userNum = scanner.nextInt();
+        System.out.println("Is the number even? " + isEven(userNum));
+    }
+}
